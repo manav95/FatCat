64f2840c88ff1dd70010ffbb241ee5b3482941da
Mabb <mdutta7@gatech.edu>
2016-09-25 07:08:22 -0400

    Loc

--- app/src/main/java/com/example/manavdutta1/hackgtproject/TitleActivity.java
+++ app/src/main/java/com/example/manavdutta1/hackgtproject/TitleActivity.java
@@ -1,5 +1,6 @@
 package com.example.manavdutta1.hackgtproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void start() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
