79ea8bc2113fa22ada93430ef45b68fa3a98106a
Mabb <mdutta7@gatech.edu>
2016-09-25 06:31:51 -0400

    Finish

--- app/src/main/java/com/example/manavdutta1/hackgtproject/MainActivity.java
+++ app/src/main/java/com/example/manavdutta1/hackgtproject/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.manavdutta1.hackgtproject;
 
+import android.hardware.SensorManager;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
 import org.andengine.entity.modifier.MoveYModifier;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.entity.scene.Scene;
@@ -29,6 +33,18 @@ import org.andengine.opengl.texture.region.TextureRegionFactory;
 import org.andengine.util.color.Color;
 import org.andengine.util.math.MathUtils;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,17 +56,20 @@ import org.andengine.entity.sprite.AnimatedSprite;
 public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouchListener {
 
     private float firstX;
+    private PhysicsWorld world;
     private ITextureRegion trumpTowerReg, leftPawReg, rightPawReg, fishboneReg, lightbulbReg;
     private int camWidth, camHeight;
-    private Sprite leftPawSprite, rightPawSprite, fishboneSprite, lightbulbSprite;
+    private Sprite leftPawSprite, rightPawSprite;
     private Scene theScene;
-    private BitmapTextureAtlas texCat, rightCat;
-    private TiledTextureRegion regCat, rightRegCat;
-    private Sprite[] fishboneSprites;
-    private Sprite[] lightbulbSprites;
+    //private BitmapTextureAtlas texCat, rightCat;
+    //private TiledTextureRegion regCat, rightRegCat;
+    private List<Sprite> fishboneSprites;
+    private List<Sprite> lightbulbSprites;
     @Override
     public EngineOptions onCreateEngineOptions() {
            DisplayMetrics metrics = new DisplayMetrics();
+           fishboneSprites = new ArrayList<>();
+           lightbulbSprites = new ArrayList<>();
            getWindowManager().getDefaultDisplay().getMetrics(metrics);
            camWidth = metrics.widthPixels;
            camHeight = metrics.heightPixels;
@@ -90,9 +109,11 @@ public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouc
         Sprite sprite;
         if (code == 0) {
             sprite = new Sprite(xPos, yPos, this.fishboneReg, getVertexBufferObjectManager());
+            fishboneSprites.add(sprite);
         }
        else {
             sprite = new Sprite(xPos, yPos, this.lightbulbReg, getVertexBufferObjectManager());
+            lightbulbSprites.add(sprite);
         }
         theScene.attachChild(sprite);
         theScene.registerTouchArea(sprite);
@@ -101,8 +122,49 @@ public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouc
                 startY, startY + 1000);
         sprite.registerEntityModifier(mody);
     }
+    private ContactListener createContactListener()
+    {
+        ContactListener contactListener = new ContactListener()
+        {
+            @Override
+            public void beginContact(Contact contact)
+            {
+                final Fixture x1 = contact.getFixtureA();
+                final Fixture x2 = contact.getFixtureB();
+                if (x1.getBody().getUserData().equals("Ground")) {
+                    Body body = x1.getBody();
+                    x2.getBody().setActive(false);
+                    body.getWorld().destroyBody(x2.getBody());
+                }
+                else {
+                    Body body = x2.getBody();
+                    x1.getBody().setActive(false);
+                    body.getWorld().destroyBody(x1.getBody());
+                }
+            }
 
-        @Override
+            @Override
+            public void endContact(Contact contact)
+            {
+
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold)
+            {
+
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse)
+            {
+
+            }
+        };
+        return contactListener;
+    }
+
+    @Override
     protected void onCreateResources() {
         try {
             ITexture background = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
@@ -150,13 +212,13 @@ public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouc
                 }
             });
 
-            texCat = new BitmapTextureAtlas(this.getTextureManager(), 136, 85, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-            regCat = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(texCat, this.getAssets(),"leftPaw.png", 0, 0, 1, 1);
-            texCat.load();
+            //texCat = new BitmapTextureAtlas(this.getTextureManager(), 136, 85, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+            //regCat = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(texCat, this.getAssets(),"leftPaw.png", 0, 0, 1, 1);
+            //texCat.load();
 
-            rightCat = new BitmapTextureAtlas(this.getTextureManager(), 136, 85, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-            rightRegCat = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(rightCat, this.getAssets(),"rightPaw.png", 0, 0, 1, 1);
-            rightCat.load();
+            //rightCat = new BitmapTextureAtlas(this.getTextureManager(), 136, 85, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+            //rightRegCat = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(rightCat, this.getAssets(),"rightPaw.png", 0, 0, 1, 1);
+            //rightCat.load();
 
             background.load();
             leftPaw.load();
@@ -174,18 +236,54 @@ public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouc
             Log.e("Error", e.getMessage());
         }
     }
-
+    private void collisionHandling(String param) {
+                Sprite paw = null;
+                if (param.equals("left")) {
+                    paw = this.leftPawSprite;
+                }
+                else {
+                    paw = this.rightPawSprite;
+                }
+                Sprite theFish = null;
+                for (Sprite s : fishboneSprites) {
+                    if (paw.collidesWith(s)) {
+                       theFish = s;
+                       break;
+                    }
+                }
+                if (theFish != null) {
+                    fishboneSprites.remove(theFish);
+                    theScene.detachChild(theFish);
+                    return;
+                }
+                else {
+                    Sprite theBulb = null;
+                    for (Sprite s : lightbulbSprites) {
+                        if (paw.collidesWith(s)) {
+                            theBulb = s;
+                            break;
+                        }
+                    }
+                    if (theBulb != null) {
+                        fishboneSprites.remove(theBulb);
+                        theScene.detachChild(theBulb);
+                        return;
+                    }
+                }
+    }
     public boolean onSceneTouchEvent(Scene pScene,  final TouchEvent pEvent) {
         if (pEvent.isActionDown()) {
            if (pEvent.getX() <= 200) {
                this.leftPawSprite = new Sprite(pEvent.getX(),pEvent.getY(), this.leftPawReg, getVertexBufferObjectManager());
                this.theScene.attachChild(this.leftPawSprite);
+               collisionHandling("left");
                Timer timer = new Timer();
                timer.schedule(new TheTask("left"), 400);
            }
             else {
                this.rightPawSprite = new Sprite(pEvent.getX(),pEvent.getY(), this.rightPawReg, getVertexBufferObjectManager());
                this.theScene.attachChild(this.rightPawSprite);
+               collisionHandling("right");
                Timer timer = new Timer();
                timer.schedule(new TheTask("right"), 400);
            }
@@ -215,34 +313,17 @@ public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouc
         int halfWidth = (int)((1.0/2.0)*camWidth);
         int wholeHeight = camHeight;
         Sprite backgroundSprite = new Sprite(0, 0, this.trumpTowerReg, getVertexBufferObjectManager());
-        //int fishX = new Random().nextInt(360)+30;
-        //int lightX = new Random().nextInt(360)+30;
-        //fishboneSprites = new Sprite[2];
-        //lightbulbSprites = new Sprite[2];
-
-        //fishboneSprites[0] = new Sprite(new Random().nextInt(360)+30,10, this.fishboneReg, getVertexBufferObjectManager());
-        //lightbulbSprites[0] = new Sprite(new Random().nextInt(360)+30,10, this.lightbulbReg, getVertexBufferObjectManager());
-        //fishboneSprites[1] = new Sprite(new Random().nextInt(360)+30,10, this.fishboneReg, getVertexBufferObjectManager());
-        //lightbulbSprites[1] = new Sprite(new Random().nextInt(360)+30,10, this.lightbulbReg, getVertexBufferObjectManager());
-
-        //for (int i = 0; i < 2; i++) {
-        //    Sprite fishSprite = fishboneSprites[i];
-        //    Sprite lightbulbSprite = lightbulbSprites[i];
-        //    float fishboneStartY = fishSprite.getY();
-        //    float lightbulbStartY = lightbulbSprite.getY();
-        //    MoveYModifier fishModifier = new MoveYModifier(2, fishboneStartY, fishboneStartY + 1000);
-        //    MoveYModifier lightbulbModifier = new MoveYModifier(2, lightbulbStartY, lightbulbStartY + 1000);
-        //    fishSprite.registerEntityModifier(fishModifier);
-        //    lightbulbSprite.registerEntityModifier(lightbulbModifier);
-        //    scene.attachChild(lightbulbSprite);
-        //    scene.attachChild(fishSprite);
-        //    scene.registerTouchArea(fishSprite);
-        //    scene.registerTouchArea(lightbulbSprite);
-        //}
+        //this.world = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+        //this.world.setContactListener(createContactListener());
+        //FixtureDef objectDef = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+        //final Rectangle ground = new Rectangle(0, 600 - 2, 400, 2, this.getVertexBufferObjectManager());
+        //PhysicsFactory.createBoxBody(this.world, ground, BodyDef.BodyType.StaticBody, objectDef);
+        //ground.setUserData("Ground");
 
         scene.setBackground(new Background(new Color(Color.BLACK)));
         scene.attachChild(backgroundSprite);
         scene.registerTouchArea(backgroundSprite);
+        //scene.attachChild(ground);
 
         scene.setTouchAreaBindingOnActionDownEnabled(true);
         scene.setOnSceneTouchListener(this);
